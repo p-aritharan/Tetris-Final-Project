45dceb7dd8bba81921f027775c11a917db8dbeb7
p-aritharan <38263960+p-aritharan@users.noreply.github.com>
2018-04-20 10:02:18 -0700

    moved images to different folder

--- project.py
+++ project.py
@@ -3,70 +3,37 @@ from pygame.locals import *
 import math
 import random
 
-Block1 = pygame.image.load("Tetris Final Project/Data/Tetris Blocks/Block1.gif")
-Block2 = pygame.image.load("Tetris Final Project/Data/Tetris Blocks/L-Block.gif")
-Block3 = pygame.image.load("Tetris Final Project/Data/Tetris Blocks/L-Block2.gif")
-Block4 = pygame.image.load("Tetris Final Project/Data/Tetris Blocks/SquareBlock.gif")
-Block5 = pygame.image.load("Tetris Final Project/Data/Tetris Blocks/StepBlock1.gif")
-Block6 = pygame.image.load("Tetris Final Project/Data/Tetris Blocks/StepBlock2.gif")
-Block7 = pygame.image.load("Tetris Final Project/Data/Tetris Blocks/T-Block.gif")
+Block1 = pygame.image.load("Block1.png")
+Block2 = pygame.image.load("L-Block.gif")
+Block3 = pygame.image.load("L-Block2.gif")
+Block4 = pygame.image.load("SquareBlock.gif")
+Block5 = pygame.image.load("StepBlock1.gif")
+Block6 = pygame.image.load("StepBlock2.gif")
+Block7 = pygame.image.load("T-Block.gif")
+sky = pygame.image.load("sky.gif")
+BLOCKS = [Block1, Block2, Block3, Block4, Block5, Block6, Block7]
 
 def startgame():
-
-pygame.init()
-width, height = 500, 750
-screen = pygame.display.set_mode((width, height))
+    pygame.init()
+    width, height = 500, 500
+    screen = pygame.display.set_mode((width, height))
 #finally got the background to load
-sky = pygame.image.load("Tetris Final Project/Data/Background/sky.gif")
-timer = 0
+    timer = 0
 
-running = 1
-done = 0
+    running = 1
+    done = 0
 
-# from random import randint
-# rblock = randint(1,7)
+    while running:
+        timer-=1
 
-# if rblock == 1:
-#     pygame.display.update(screen.blit(Block1))
-# if rblock == 2:
-#     pygame.display.update(screen.blit(Block2))
-# if rblock == 3:
-#     pygame.display.update(screen.blit(Block3))
-# if rblock == 4:
-#     pygame.display.update(screen.blit(Block4))
-# if rblock == 5:
-#     pygame.display.update(screen.blit(Block5))
-# if rblock == 6:
-#     pygame.display.update(screen.blit(Block6))
-# if rblock == 7:
-#     pygame.display.update(screen.blit(Block7))
+        screen.fill(0)
 
-
-while running:
-    timer-=1
-
-    screen.fill(0)
-
-    for x in range(width//sky.get_width()+1):
-        for y in range(height//sky.get_height()+1):
+        for x in range(width//sky.get_width()+1):
+            for y in range(height//sky.get_height()+1):
              pygame.display.update(screen.blit(sky,(x*100,y*100)))
     
-    startgame()
-
-
-    # if timer==0:
-    #     rblock.append([640, random.randint(50,430)])
-    #     timer=100-(timer*2)
-    #     if timer1>=35:
-    #         timer1=35
-    #     else:
-    #         timer1+=5
-    # index=0
-    # for badguy in rblock:
-    #     if badguy[0]<-64:
-    #         rblock.pop(index)
-    #     badguy[0]-=7
-
+    
+startgame()
 
 #4/12 just got github as well as collaboration working
 
--- test.py
+++ test.py
@@ -1,32 +1,28 @@
-# took this code from https://stackoverflow.com/questions/44468625/how-to-make-a-button-in-pygame-display-and-update-images
-# using this code we can then add the following
-#1. startgame button which is activated with mouseclick
-#2. scroll to rotate blocks and left-click to move block down
 import sys
 import pygame
 
-# Three images.
-IMG1 = pygame.Surface((100, 100))
-IMG1.fill((240, 240, 240))
-pygame.draw.circle(IMG1, (0, 0, 0), (50, 50), 10)
-IMG2 = pygame.Surface((100, 100))
-IMG2.fill((240, 240, 240))
-pygame.draw.circle(IMG2, (0, 0, 0), (25, 25), 10)
-pygame.draw.circle(IMG2, (0, 0, 0), (75, 75), 10)
-IMG3 = pygame.Surface((100, 100))
-IMG3.fill((240, 240, 240))
-pygame.draw.circle(IMG3, (0, 0, 0), (20, 20), 10)
-pygame.draw.circle(IMG3, (0, 0, 0), (50, 50), 10)
-pygame.draw.circle(IMG3, (0, 0, 0), (80, 80), 10)
+# def rot_center(image, angle):
+#     """rotate an image while keeping its center and size"""
+#     orig_rect = image.get_rect()
+#     rot_image = pygame.transform.rotate(image, angle)
+#     rot_rect = orig_rect.copy()
+#     rot_rect.center = rot_image.get_rect().center
+#     rot_image = rot_image.subsurface(rot_rect).copy()
+#     return rot_image
+
+IMG1 = pygame.image.load("Block1.gif")
+# two rotations
+IMG100 = IMG1
+IMG200 = pygame.transform.rotate(IMG1, 90)
 # Put the images into a list or tuple.
-IMAGES = [IMG1, IMG2, IMG3]
+IMAGES = [IMG100, IMG200]
 
 def main():
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((500, 500))
     clock = pygame.time.Clock()
     bg_color = pygame.Color(30, 40, 60)
     # Use this rect for the collision detection and blitting.
-    img_rect = IMG1.get_rect(topleft=(200, 200))
+    img_rect = IMG1.get_rect(center=(200, 200))
     img_index = 0
     done = False
 
@@ -54,7 +50,3 @@ if __name__ == '__main__':
     main()
     pygame.quit()
     sys.exit()
-
-
-
-
